2653f1bdb0c35130f37d971a7593c91b9bd5319b
Brian Crandall <bcranda@engr-ras-100.eos.ncsu.edu>
2020-02-03 17:35:04 -0500

    hw_4

--- hw_4/hw4_q1c.v
+++ hw_4/hw4_q1c.v
@@ -1,6 +1,6 @@
 module hw4_q1c;
 
-  integer i;
+  integer i, j;
   reg [15:0] A, B;
   reg Cin = 1'b0;
   wire [16:0] S;
@@ -9,11 +9,12 @@ module hw4_q1c;
 
   initial
   begin
-    $monitor( $time, " %d + %d = %d", A, B, S);
-    for( i=0; i<1000; i=i+1 )
-    begin
-      #10 A = i;
-          B = i;
+    $monitor( $time, " %d + %d = %d%d%d%d", A, B, S[16], S[15:12], S[11:8], S[7:4], S[3:0]); 
+    for( i=0; i<10; i=i+1 )
+      for( j=0; j<10; j=j+1 )
+        begin
+          #10 A = i;
+              B = j;
     end
   end
 
